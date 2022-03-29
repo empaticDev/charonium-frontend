129cd81c4408abfa78f82b6194900eafef86d168
moris@empatic-ux.com <UG0_BphW}V$RPf-;[[,zp4rY)-:7Q9r2>
2022-03-29 19:39:42 +0200

    feat: Init storybook base

---
+++ components/layout/header/header.story.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import LayoutHeader from './header'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+	title: 'Layout/Header',
+	component: LayoutHeader,
+	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <LayoutHeader {...args} />
+
+export const Primary = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+	primary: true,
+	label: 'Button',
+}
