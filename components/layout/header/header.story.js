76a05b50626c3bb3795814ac4cba8dda22ec1587
moris@empatic-ux.com <UG0_BphW}V$RPf-;[[,zp4rY)-:7Q9r2>
2022-03-30 13:42:12 +0200

    story[header]: Update Props

--- components/layout/header/header.story.js
+++ components/layout/header/header.story.js
@@ -14,7 +14,4 @@ const Template = (args) => <LayoutHeader {...args} />
 
 export const Primary = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-	primary: true,
-	label: 'Button',
-}
+Primary.args = {}
