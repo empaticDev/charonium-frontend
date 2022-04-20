d14a6755905e46d49ee8ec970bc062bba4f2ec4e
moelp <UG0_BphW}V$RPf-;[[,zp4rY)-:7Q9r2>
2022-04-21 01:54:37 +0200

    feat(blocktextMedia): Add markdown rendering basic styling

--- components/block/textMedia/textMedia.js
+++ components/block/textMedia/textMedia.js
@@ -1,22 +1,38 @@
 import classNames from 'classnames/bind'
-import styles from './style.module.scss'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import remarkHtml from 'remark-html'
+import rehypeRaw from 'rehype-raw'
+import rehypeSanitize from 'rehype-sanitize'
 import Image from 'next/image'
+
+import styles from './style.module.scss'
 import { BlockWrapper } from '@components/block'
+import { PartialText } from '@components/partial'
+import { SharedBlockManager } from '@components/shared'
 
 let cx = classNames.bind(styles)
 
-export default function TextMedia({ title, content }) {
+export default function TextMedia({ title, label, content, ctas }) {
 	let className = cx({
-		'text-media': true,
+		'textMedia': true,
 	})
+
+	const hasLink = ctas != null ? true : false;
 
 	return (
 		<BlockWrapper className={className}>
 			<div className={className}>
-				{title && <h2>{title}</h2>}
-
-				{content}
+				{label && <p className={styles.pretitle}>{label}</p>}
+				{title && <h2 className={styles.title}>{title}</h2>}
+				<div className={styles.content}>
+					<ReactMarkdown children={content} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw, [rehypeSanitize, {tagNames: ['p','del','strong','em','a','b','blockquote','ul','ol','li','u','code']}]] }/>
+				</div>
+				<div className={styles.footer}>
+					{ hasLink && <SharedBlockManager blocks={ctas} /> }
+				</div>
 			</div>
 		</BlockWrapper>
 	)
 }
+
