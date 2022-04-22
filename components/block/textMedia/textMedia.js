8f33f78a80a12fa5ac008d618b482b51f765af33
zlwilson <zackinator@gmail.com>
2022-04-22 15:50:09 +0200

    feat: text block alignment

--- components/block/textMedia/textMedia.js
+++ components/block/textMedia/textMedia.js
@@ -11,26 +11,56 @@ import { SharedBlockManager } from '@components/shared'
 
 let cx = classNames.bind(styles)
 
-export default function TextMedia({ title, label, content, ctas }) {
+export default function TextMedia({ title, label, content, ctas, alignment }) {
 	let className = cx({
-		'textMedia': true,
+		textMedia: true,
+		'content-left': alignment === 'right' ? true : false,
 	})
 
-	const hasLink = ctas != null ? true : false;
+	const hasLink = ctas != null ? true : false
 
 	return (
 		<BlockWrapper className={className}>
+			{alignment === 'right' ? 'right' : 'left'}
+
 			<div className={className}>
-				{label && <p className={styles.pretitle}>{label}</p>}
-				{title && <h2 className={styles.title}>{title}</h2>}
-				<div className={styles.content}>
-					<ReactMarkdown children={content} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw, [rehypeSanitize, {tagNames: ['p','del','strong','em','a','b','blockquote','ul','ol','li','u','code']}]] }/>
-				</div>
-				<div className={styles.footer}>
-					{ hasLink && <SharedBlockManager blocks={ctas} /> }
+				<div>image</div>
+				<div>
+					{label && <p className={styles.pretitle}>{label}</p>}
+					{title && <h2 className={styles.title}>{title}</h2>}
+					<div className={styles.content}>
+						<ReactMarkdown
+							children={content}
+							remarkPlugins={[remarkGfm]}
+							rehypePlugins={[
+								rehypeRaw,
+								[
+									rehypeSanitize,
+									{
+										tagNames: [
+											'p',
+											'del',
+											'strong',
+											'em',
+											'a',
+											'b',
+											'blockquote',
+											'ul',
+											'ol',
+											'li',
+											'u',
+											'code',
+										],
+									},
+								],
+							]}
+						/>
+					</div>
+					<div className={styles.footer}>
+						{hasLink && <SharedBlockManager blocks={ctas} />}
+					</div>
 				</div>
 			</div>
 		</BlockWrapper>
 	)
 }
-
