b81721e5fbd4da48e85d7bfb90aebd1ea3271080
zlwilson <zackinator@gmail.com>
2022-05-04 12:15:16 +0200

    feat(accordion): basic functionality

--- components/partial/accordion/accordion.js
+++ components/partial/accordion/accordion.js
@@ -1,17 +1,44 @@
 import classNames from 'classnames/bind'
 import styles from './accordion.module.scss'
+import FeatherIcon from 'feather-icons-react'
 
-export default function Accordion({title, content}) {
+import { useState } from 'react'
+
+export default function Accordion({ title, content }) {
 	let cx = classNames.bind(styles)
 
 	let className = cx({
 		accordion: true,
 	})
 
+	const [expanded, setExpanded] = useState(false)
+
+	function toggle() {
+		setExpanded(!expanded)
+	}
+
+	var contentClass = cx({
+		content: true,
+		show: expanded,
+		hide: !expanded,
+	})
+
+	var titleClass = cx({
+		title: true,
+		expanded: expanded,
+		collapsed: !expanded,
+	})
+
 	return (
 		<div className={className}>
-			{title}
-            {content}
+			<button className={titleClass} onClick={toggle}>
+				{title}
+				{/* <FeatherIcon icon={'chevron-down'} /> */}
+			</button>
+
+			<div className={contentClass}>
+				<p>{content}</p>
+			</div>
 		</div>
 	)
 }
