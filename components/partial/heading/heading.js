1bb9849a651c28adcf743b3d44eaa5e22cdc9691
moelp <UG0_BphW}V$RPf-;[[,zp4rY)-:7Q9r2>
2022-05-06 01:37:09 +0200

    feat(components): Render Headings in right markup

--- components/partial/heading/heading.js
+++ components/partial/heading/heading.js
@@ -3,22 +3,27 @@ import styles from './heading.module.scss'
 
 let cx = classNames.bind(styles)
 
-export default function Heading({ label, title, heading }) {
+export default function Heading({ label, title, heading, as }) {
 
 	heading = heading ? heading : 'h2'
 
 	let className = cx({
-		heading: true,
+		footer: heading === 'footer' ? true : false,
+	})
+
+	let classHeading = cx({
+		title: true,
 		h1: heading === 'h1' ? true : false,
 		h2: heading === 'h2' ? true : false,
 		h3: heading === 'h3' ? true : false,
-		footer: heading === 'footer' ? true : false,
 	})
+
+	const H = as ? as : 'h2'
 
 	return (
 		<div className={className}>
 			{label && <p className={styles.caption}>{label}</p>}
-			{title && <div className={styles.title}>{title}</div>}
+			{title && <H className={classHeading}>{title}</H>}
 		</div>
 	)
 }
