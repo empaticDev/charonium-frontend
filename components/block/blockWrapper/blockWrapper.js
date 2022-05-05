d356512de86324bfbcdb51704f431de2dcbd5b6e
zlwilson <zackinator@gmail.com>
2022-05-05 17:22:16 +0200

    fix(blocks): full width

--- components/block/blockWrapper/blockWrapper.js
+++ components/block/blockWrapper/blockWrapper.js
@@ -3,28 +3,37 @@ import styles from './block.module.scss'
 
 let cx = classNames.bind(styles)
 
-export default function BlockWrapper({ children, spacing, id, anchor }) {
+export default function BlockWrapper({
+	children,
+	spacing,
+	id,
+	anchor,
+	fullWidth,
+}) {
 	let className = cx({
 		block: true,
-		'spacing--small': spacing != undefined && spacing === 'small' ? true : false,
+		'spacing--small':
+			spacing != undefined && spacing === 'small' ? true : false,
 		'spacing--none': spacing != undefined && spacing === 'none' ? true : false,
+	})
+
+	let innerClass = cx({
+		block__inner: fullWidth != undefined && fullWidth ? false : true,
 	})
 
 	let atts = {}
 	let linkFormat = ''
-	if(anchor) {
+	if (anchor) {
 		linkFormat = anchor.name.toLowerCase()
-    	linkFormat = encodeURIComponent(linkFormat).replace(/%20/g, '-')
-    	linkFormat = `${linkFormat}-${id}`
+		linkFormat = encodeURIComponent(linkFormat).replace(/%20/g, '-')
+		linkFormat = `${linkFormat}-${id}`
 
 		atts.id = linkFormat
 	}
 
 	return (
-		<section {...atts}  className={className}>
-			<div className={styles.block__inner}>
-				{children}
-			</div>		
+		<section {...atts} className={className}>
+			<div className={innerClass}>{children}</div>
 		</section>
 	)
 }
