8e55acd551bc46183bbe00c33b00359fce8125a5
zlwilson <zackinator@gmail.com>
2022-05-09 10:28:08 +0200

    feat(images): fade in

--- components/partial/teamMember/teamMember.js
+++ components/partial/teamMember/teamMember.js
@@ -2,6 +2,8 @@ import classNames from 'classnames/bind'
 import styles from './teamMember.module.scss'
 import Image from 'next/image'
 
+import { useState } from 'react'
+
 let cx = classNames.bind(styles)
 
 export default function TeamMember({
@@ -16,11 +18,39 @@ export default function TeamMember({
 		teammember: true,
 	})
 
+	const [avatarLoaded, setAvatarLoaded] = useState(false)
+	const [imageLoaded, setImageLoaded] = useState(false)
+
+	let avatarClass = cx({
+		avatarWrapper: true,
+		notloaded: !avatarLoaded,
+		loaded: avatarLoaded,
+	})
+
+	let imageClass = cx({
+		imagewrapper: true,
+		notloaded: !imageLoaded,
+		loaded: imageLoaded,
+	})
+
+	const handleAvatarLoad = (event) => {
+		const target = event.target
+
+		if (target.src.indexOf('data:image/gif;base64') < 0) {
+			setAvatarLoaded(true)
+		}
+	}
+
+	const handleImageLoad = (event) => {
+		const target = event.target
+
+		if (target.src.indexOf('data:image/gif;base64') < 0) {
+			setImageLoaded(true)
+		}
+	}
+
 	const avatarURL = avatar.data.attributes.url
 	const imageURL = image.data.attributes.url
-
-	const avatarH = 300
-	const avatarW = avatarH * 0.8
 
 	return (
 		<>
@@ -30,14 +60,34 @@ export default function TeamMember({
 					<div className={styles.box}></div>
 				</div>
 				{avatar && (
-					<div className={styles.avatarwrapper}>
-						<Image src={avatarURL} width={avatarW} height={avatarH} />
+					<div className={avatarClass}>
+						<Image
+							className={styles.avatar}
+							src={avatarURL}
+							layout="fill"
+							onLoad={(event) => {
+								const target = event.target
+								if (target.src.indexOf('data:image/gif;base64') < 0) {
+									setAvatarLoaded(true)
+								}
+							}}
+						/>
 					</div>
 				)}
 				<div className={styles.description}>
 					{image && (
-						<div className={styles.imagewrapper}>
-							<Image src={imageURL} layout="fill" />
+						<div className={imageClass}>
+							<Image
+								className={styles.image}
+								src={imageURL}
+								layout="fill"
+								onLoad={(event) => {
+									const target = event.target
+									if (target.src.indexOf('data:image/gif;base64') < 0) {
+										setImageLoaded(true)
+									}
+								}}
+							/>
 						</div>
 					)}
 					<div className={styles.info}>
