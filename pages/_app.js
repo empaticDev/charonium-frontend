a40618874339df70066a12c09102f9e72cd9c61a
zlwilson <zackinator@gmail.com>
2022-06-02 14:13:12 +0200

    fix(cookies): scrolling before accept + overlay

--- pages/_app.js
+++ pages/_app.js
@@ -9,64 +9,59 @@ const cookieMessage =
 function MyApp({ Component, pageProps }) {
 	const [cookies, setCookies] = useState(false)
 
-	useEffect(() => {
-		cookies
-			? (document.body.style.overflow = 'scroll')
-			: (document.body.style.overflow = 'hidden')
-
-		// document.getElementById('overlay').style.display = 'fixed'
-	}, [cookies])
+	console.log(cookies)
 
 	return (
 		<>
-			<CookieBanner
-				message={cookieMessage}
-				wholeDomain={true}
-				onAccept={() => {
-					setCookies(true)
-				}}
-				policyLink={'/gdpr'}
-				necessaryOptionText={'Necessary'}
-				managePreferencesButtonText={'Manage Preferences'}
-				showPreferencesOption={false}
-				showStatisticsOption={true}
-				showMarketingOption={true}
-				onAcceptPreferences={() => {}}
-				onAcceptStatistics={() => {}}
-				onAcceptMarketing={() => {}}
-				styles={{
-					button: {
-						backgroundColor: '#57c5fd',
-						color: '#121212',
-						padding: '0.5em 1.5em 0.5em 1.5em',
-						marginLeft: '0.5em',
-						borderRadius: '0.5em',
-						fontFamily: 'BarlowCondensed, sans-serif',
-					},
-					policy: {
-						color: '#121212',
-						padding: '1em 1em 1em 1em',
-						fontFamily: 'BarlowCondensed, sans-serif',
-						textDecoration: 'underline',
-					},
-					dialog: {
-						boxShadow: '0em 2em 10em 60em rgb(0,0,0,0.5)',
-						backgroundColor: '#FFFFFF',
-						position: 'fixed',
-						zIndex: '100000',
-						width: '100%',
-						padding: '1em 1em 1em 1em',
-					},
-					optionLabel: {
-						color: '#121212',
-						paddingLeft: '0.5em',
-					},
-					checkbox: {
-						borderRadius: '4px',
-					},
-				}}
-			/>
-			{/* <div id="overlay" className="cookie-overlay"></div> */}
+			<div>
+				<CookieBanner
+					message={cookieMessage}
+					wholeDomain={true}
+					onAccept={() => {
+						setCookies(true)
+					}}
+					policyLink={'/gdpr'}
+					necessaryOptionText={'Necessary'}
+					managePreferencesButtonText={'Manage Preferences'}
+					showPreferencesOption={false}
+					showStatisticsOption={true}
+					showMarketingOption={true}
+					onAcceptPreferences={() => {}}
+					onAcceptStatistics={() => {}}
+					onAcceptMarketing={() => {}}
+					styles={{
+						button: {
+							backgroundColor: '#57c5fd',
+							color: '#121212',
+							padding: '0.5em 1.5em 0.5em 1.5em',
+							marginLeft: '0.5em',
+							borderRadius: '0.5em',
+							fontFamily: 'BarlowCondensed, sans-serif',
+						},
+						policy: {
+							color: '#121212',
+							padding: '1em 1em 1em 1em',
+							fontFamily: 'BarlowCondensed, sans-serif',
+							textDecoration: 'underline',
+						},
+						dialog: {
+							backgroundColor: '#FFFFFF',
+							position: 'fixed',
+							zIndex: '10000',
+							width: '100%',
+							padding: '1em 1em 1em 1em',
+						},
+						optionLabel: {
+							color: '#121212',
+							paddingLeft: '0.5em',
+						},
+						checkbox: {
+							borderRadius: '4px',
+						},
+					}}
+				/>
+			</div>
+			<div id="overlay" className={cookies ? 'hide' : 'cookie-overlay'}></div>
 			<LayoutHeader {...pageProps} />
 			<LayoutDefault>
 				<Component {...pageProps} />
